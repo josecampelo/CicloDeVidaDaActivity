0c948a09031abfe50230d289d72ae6e3042980a4
José Campelo <josecampelojr@gmail.com>
2023-06-27 20:47:53 -0300

    Criação da lógica para o botão encerrar a segunda activity caso o usuário clique no botão Sair.

--- app/src/main/java/com/campelo/ciclodevidadaactivity/SegundaActivity.kt
+++ app/src/main/java/com/campelo/ciclodevidadaactivity/SegundaActivity.kt
@@ -2,10 +2,19 @@ package com.campelo.ciclodevidadaactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SegundaActivity : AppCompatActivity() {
+
+    private lateinit var btnSair: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda)
+
+        btnSair = findViewById(R.id.btnSair)
+        btnSair.setOnClickListener {
+            finish()
+        }
     }
 }
