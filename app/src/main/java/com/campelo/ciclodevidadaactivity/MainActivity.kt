a1bb4053500892fbc3cb956a8b4e759e9d791774
José Campelo <josecampelojr@gmail.com>
2023-06-27 20:45:14 -0300

    Criação da navegação com intent da Activity principal para a Activity secundária.

--- app/src/main/java/com/campelo/ciclodevidadaactivity/MainActivity.kt
+++ app/src/main/java/com/campelo/ciclodevidadaactivity/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.campelo.ciclodevidadaactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnEntrar: Button
+    private lateinit var btnEncerrar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnEntrar = findViewById(R.id.btnEntrar)
+        btnEncerrar = findViewById(R.id.btnEncerrar)
+
+        btnEntrar.setOnClickListener {
+            val intent = Intent(this, SegundaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnEncerrar.setOnClickListener {
+            finish()
+        }
     }
 }
