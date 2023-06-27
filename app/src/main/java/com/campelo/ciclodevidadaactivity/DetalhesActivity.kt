5f021d5ff6493430430f90fb7ed44631256edb1e
José Campelo <josecampelojr@gmail.com>
2023-06-27 20:59:01 -0300

    Criação da lógica para o botão voltar

--- app/src/main/java/com/campelo/ciclodevidadaactivity/DetalhesActivity.kt
+++ app/src/main/java/com/campelo/ciclodevidadaactivity/DetalhesActivity.kt
@@ -2,10 +2,19 @@ package com.campelo.ciclodevidadaactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetalhesActivity : AppCompatActivity() {
+
+    private lateinit var btnVoltar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes)
+
+        btnVoltar = findViewById(R.id.btnVoltar)
+        btnVoltar.setOnClickListener {
+            finish()
+        }
     }
 }
